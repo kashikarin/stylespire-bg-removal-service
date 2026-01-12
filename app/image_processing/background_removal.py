58ad4e5ea39a58e0b467dfcbc95995ac0c6dffe7
kashikarin <karinkashi@gmail.com>
2026-01-13 00:00:47 +0200

    fix u2net session issue

--- app/image_processing/background_removal.py
+++ app/image_processing/background_removal.py
@@ -1,11 +1,23 @@
-from rembg import remove
+
+from rembg import remove, new_session
 from PIL import Image
+from io import BytesIO
+
+session = new_session("u2net")
 
 def remove_background(image: Image.Image) -> Image.Image:
     try:
         if image.mode != "RGBA":
             image = image.convert("RGBA")
+
+            input_buffer = BytesIO()
+            image.save(input_buffer, format="PNG")
+
+            output_bytes = remove(input_buffer.getvalue(), session=session)
+
+            return Image.open(BytesIO(output_bytes)).convert("RGBA")
+        
         return remove(image)
-    except Exception:
-        raise RuntimeError("Background removal failed")
+    except Exception as e:
+        raise RuntimeError(f"Background removal failed: {e}")
 
